b35ed64cf2afd1a14377197caf9e080cb2248611
rakhfa <ra693@drexel.edu>
2019-10-27 14:44:29 -0400

    added my_app application

--- my_app/tests.py
+++ my_app/tests.py
@@ -7,3 +7,6 @@ class MonsterModelTest(TestCase):
 
     def test_monster_content(self):
         self.assertEquals("Godzilla", "Godzilla")
+
+    def test_monster_content2(self):
+        self.assertEquals("Godzilla", "Godzillaa")
